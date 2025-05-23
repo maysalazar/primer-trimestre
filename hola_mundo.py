29b0f01fd8652c9306cb8a3c0fdb8dd32e8fc14a
mairon salazar <maironsalazar16@gmail.com>
2025-05-23 00:10:45 -0500

    interaccion con el usuario en el documento hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,5 +1,9 @@
-print("hola mundo")
-suma = 3+4
-print(f"la suma de los numeros es,{suma}")
+# print("hola mundo")
+# suma = 3+4
+# print(f"la suma de los numeros es,{suma}")
 
-print("hola colombia") # esto es un print
+# print("hola colombia") # esto es un print
+
+nombre = int(input("ingresar un numero al sistema: "))
+
+print(nombre)
